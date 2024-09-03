af4cc24f30689b62e5eebc7635daf2b169302a60
Agustin Arce <agustinarceprincich@gmail.com>
2024-09-03 19:32:42 -0400

    refactor (common): make controller an abstract class

--- aiostipy/common/classes/controller.py
+++ aiostipy/common/classes/controller.py
@@ -1,6 +1,8 @@
-from typing import Optional
+from abc import ABC
+from typing import List, Optional
 
 
-class Controller:
+class Controller(ABC):
 
     prefix: Optional[str] = "/"
+    tags: Optional[List[str]] = None
